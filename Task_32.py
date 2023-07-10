9bde67abbf33a16357b1f974ae4d2cea2cec89f0
Dmitriy <Dipan187@live.ru>
2023-07-10 13:33:30 +0300

    Task 32

---
+++ Task_32.py
@@ -0,0 +1,13 @@
+# Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону 
+# (т.е. не меньше заданного минимума и не больше заданного максимума)
+
+
+my_list = [int(i) for i in input('Введите элементы массива через пробел: ').split()]
+minimum = int(input('Введите минимальное число: '))
+mаximum = int(input('Введите максимальное число: '))
+resalt = []
+for ind, value in enumerate(my_list):
+    if minimum <= value <= mаximum:
+        resalt.append(ind)
+print(* resalt, sep=', ')
+
